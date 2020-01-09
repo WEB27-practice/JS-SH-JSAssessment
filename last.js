91bc6d3b45c54f37e3fd6a510f42e5061e4fdea1
seanaleid <seanaleid@gmail.com>
2020-01-08 23:14:10 -0500

    Adds code from SH

--- last.js
+++ last.js
@@ -21,8 +21,24 @@ const obj = {
       "The Empire Strikes Back",
       "Return of the Jedi",
       "Revenge of the Sith",
-      "The Force Awakens"
+      "The Force Awakens",
+      1
     ],
 }
 
+function nthFilm(filmNumber) {
+    if(1 <= filmNumber && filmNumber <= 3) {
+        return obj.films[filmNumber - 1];
+    } else {
+        return 'Random answer with Error included'
+    }
+};
 
+// gives you 
+console.log(nthFilm(1));
+
+
+console.log(obj.films.length);
+
+// gives you the last item in the array
+console.log(obj.films[obj.films.length -1]);
